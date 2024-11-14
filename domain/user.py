76ac113612ea91739a8ad0f761fa2f0bae59bec8
Wilknis Deyvis <wilknis.oliveira@truework.com.br>
2024-11-14 16:08:02 -0300

    initial

---
+++ domain/user.py
@@ -0,0 +1,18 @@
+from domain.entity import Entity
+from sqlalchemy import Column, String
+from .base import Base
+
+
+class User(Entity, Base):
+    __tablename__ = 'users'
+    name = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    password = Column(String(50), nullable=False)
+
+    def __init__(self, name: str, email: str, password: str):
+        super().__init__()
+        self.name: str = name
+        self.email: str = email
+        self.password: str = password
+
+
